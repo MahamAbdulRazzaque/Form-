f67dbd3c3e2e90780cf4b5da3b1bcf910fc69059
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-12-20 17:10:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// function data(){
+//     let inputname = document.getElementById("inputname").value;
+//     let inputemail = document.getElementById("inputemail").value;
+//     let inputpassword = document.getElementById("inputpassword").value;
+//     alert("Sign in Successfully");
+//     document.write("Information is    " , inputname ,"<br>", inputemail ,"<br>", inputpassword);
+// }
+
+
+
+//  function fillCity(){
+//     let cityName = "";
+//     let zipEntered = document.getElementById("zip").value;
+//     if (zipEntered == 123){
+//         cityName = "Karachi"
+//     }
+//     else if (zipEntered == 456){
+//         cityName = "Lahore"
+//     }
+//     else if (zipEntered == 789){
+//         cityName = "Esia"
+//     }    
+//     document.getElementById("city").value = cityName;
+// }
+
